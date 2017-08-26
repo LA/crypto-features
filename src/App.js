5292c24221345d859c353060b6cd2da121b5205b
Adar <theadarbutel@gmail.com>
2017-08-25 21:41:44 -0700

    Add top 10 coins to footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import format from 'number-format.js';
+import request from 'request';
+
 class App extends Component {
   constructor() {
     super();
     const feed = [
       {
-        title: 'Ex-lottery worker who rigged winnings gets 25 years in prison',
-        source: 'washingtonpost.com',
+        title: "Ethereum's 'Metropolis' Upgrade Could Still Be Months Away                                        ",
+        source: 'www.coindesk.com',
         author: 'Marky Mark',
         timeAgo: '25min',
       },
       {
-        title: 'Ex-lottery worker who rigged winnings gets 25 years in prison',
+        title: 'Bitcoin Prices Rise But Fall Short of All-Time High',
         source: 'cnn.com',
         author: 'Marky Mark',
         timeAgo: '25min',
@@ -154,15 +157,28 @@ class App extends Component {
 
     this.state = {
       feed: feed,
+      prices: [],
     };
 
     document.title = 'News'
   }
 
+  componentDidMount() {
+    let prices = [];
+    const qs = {limit: 10};
+    request({url: 'https://api.coinmarketcap.com/v1/ticker/', qs: qs}, (err, response, body) => {
+      if (err) { console.error(err); return; }
+      const json = JSON.parse(body);
+      json.forEach((coin) => prices.push(coin));
+      console.log(prices);
+      this.setState({prices: prices});
+    });
+  }
+
   render() {
     return (
       <center style={{backgroundColor: 'black'}}>
-        <table className='site-container' style={{paddingTop: 10}} width='100%' height='100%'>
+        <table className='table' style={{paddingTop: 10}}>
           <tbody>
             <tr>
 
@@ -182,7 +198,7 @@ class App extends Component {
                       </td>
                       <td style={{textAlign: 'right', paddingRight: 4}}>
                         <span className='powered-by'>
-                          powered by <a target='_blank' style={{color: 'gray'}} href='https://newsapi.org/'>NewsAPI.org</a>
+                          semi-powered by <a target='_blank' style={{color: 'gray'}} href='https://newsapi.org/'>NewsAPI.org</a>
                         </span>
                       </td>
                     </tr>
@@ -220,14 +236,6 @@ class App extends Component {
                             </td>
                             <td></td>
                           </tr>
-                          <tr>
-                            <td colSpan='2'></td>
-                            <td>
-                              <span style={{color: 'gray', fontSize: 10}}>
-                                {item.author}
-                              </span>
-                            </td>
-                          </tr>
                           <tr style={{height: 5}}></tr>
                         </tr>
                       );
@@ -235,8 +243,25 @@ class App extends Component {
                   </tbody>
                 </table>
               </td>
-
             </tr>
+
+            <div style={{marginLeft: 'auto'}}>
+              <div style={{marginTop: 25, marginLeft: 25, display: 'inline-block'}}>
+                {this.state.prices.map((coin, index) => {
+                  return (
+                    <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>{coin.name}: {coin.price_usd} USD ({format('#,###.', coin.market_cap_usd)} USD)</p>
+                  );
+                })}
+                <p className='powered-by' style={{fontSize: 10, textAlign: 'left'}}>
+                  powered by <a target='_blank' style={{color: 'gray'}} href='https://coinmarketcap.com/'>CoinMarketCap.com</a>
+                </p>
+              </div>
+              <div style={{marginTop: 25, marginLeft: 25, display: 'inline-block'}}>
+                <p className='powered-by' style={{fontSize: 10}}>
+                  send me feedback <a target='_blank' style={{color: 'gray'}} href='https://twitter.com/adarbutel'>@adarbutel</a> or <a target='_blank' style={{color: 'gray'}} href='mailto:a@cryptofeatures.com'>a@cryptofeatures.com</a>
+                </p>
+              </div>
+            </div>
           </tbody>
         </table>
       </center>
