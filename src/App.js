95d1927130401839671449b3245d1ae65551fdf5
Adar <theadarbutel@gmail.com>
2017-09-15 00:11:08 -0700

    Add coinmarketcap global data and my wallets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import btcQR from './btcQR.png';
+import ethQR from './ethQR.png';
+import ltcQR from './ltcQR.png';
+import xmrQR from './xmrQR.png';
 import './App.css';
 
 import format from 'number-format.js';
@@ -12,6 +16,7 @@ class App extends Component {
     this.state = {
       feed: [],
       prices: [],
+      globalData: {},
     };
 
     document.title = 'News'
@@ -19,12 +24,28 @@ class App extends Component {
 
   componentDidMount() {
     let prices = [];
-    const qs = {limit: 10};
+    const qs = {limit: 50};
     request({url: 'https://api.coinmarketcap.com/v1/ticker/', qs: qs}, (err, response, body) => {
       if (err) { console.error(err); return; }
       const json = JSON.parse(body);
       json.forEach((coin) => prices.push(coin));
       this.setState({prices});
+    });
+
+    request({url: 'https://api.coinmarketcap.com/v1/global/'}, (err, res, body) => {
+      if (err) { return console.error(err); }
+      const json = JSON.parse(body);
+      const totalMarketCap = json['total_market_cap_usd'];
+      const total24HVolume = json['total_24h_volume_usd'];
+      const btcPercentage = json['bitcoin_percentage_of_market_cap'];
+      const activeCurrencies = json['active_currencies'];
+      const globalData = {
+        totalMarketCap: totalMarketCap,
+        total24HVolume: total24HVolume,
+        btcPercentage: btcPercentage,
+        activeCurrencies: activeCurrencies,
+      };
+      this.setState({globalData});
     });
 
     request({url: 'https://api.cryptofeatures.com'}, (err, response, body) => {
@@ -109,13 +130,56 @@ class App extends Component {
             </tr>
 
             <div style={{marginLeft: 'auto'}}>
-              <div style={{marginTop: 25, marginLeft: 25, display: 'inline-block'}}>
+              <div style={{marginTop: 50, marginLeft: 25, display: 'inline-block'}}>
+                <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
+                  Global Data
+                </p>
+                <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
+                  {
+                    'Total Market Cap (USD): '
+                    + '$'
+                    + format('#,###.', this.state.globalData.totalMarketCap)
+                  }
+                </p>
+                <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
+                  {
+                    '24 Hour Volume (USD): '
+                    + '$'
+                    + format('#,###.', this.state.globalData.total24HVolume)
+                  }
+                </p>
+                <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
+                  {
+                    'BTC Percentage of Market Cap: '
+                    + this.state.globalData.btcPercentage
+                    + '%'
+                  }
+                </p>
+                <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
+                  {
+                    'Active Currencies: '
+                    + this.state.globalData.activeCurrencies
+                  }
+                </p>
+              </div>
+            </div>
+
+            <div style={{marginLeft: 'auto'}}>
+              <div style={{marginTop: 15, marginLeft: 25, display: 'inline-block'}}>
                 <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
                   Name: BTC Equiv ($USD) - $USD MKT Cap - % Change 24H
                 </p>
                 {this.state.prices.map((coin, index) => {
+                  const price = Number(coin.price_usd);
+                  console.log(coin.name, coin.price_btc);
+                  const coinPrice = (price > 1) ? format('#,###.00', price) : price;
+
+                  const coinMarketCap = format('#,###.', coin.market_cap_usd);
+
+                  const btcEquiv = coin.price_btc;
+
                   return (
-                    <p key={index} style={{color: 'white', textAlign: 'left', fontSize: 10}}>{coin.name}: {format('#.00000000', coin.price_btc)} (${format('#,###.00', coin.price_usd)}) - ${format('#,###.', coin.market_cap_usd)} - <a style={{color: (coin.percent_change_24h[0] === '-') ? 'red' : 'green'}} >{(coin.percent_change_24h[0] === '-') ? '↓' + coin.percent_change_24h : '↑+' + coin.percent_change_24h}%</a> 24H</p>
+                    <p key={index} style={{color: 'white', textAlign: 'left', fontSize: 10}}>{index+1}. {coin.name}: {btcEquiv} (${coinPrice}) - ${coinMarketCap} - <a style={{color: (coin.percent_change_24h[0] === '-') ? 'red' : 'green'}} >{(coin.percent_change_24h[0] === '-') ? '↓' + coin.percent_change_24h : '↑+' + coin.percent_change_24h}%</a> 24H</p>
                   );
                 })}
                 <p className='powered-by' style={{fontSize: 10, textAlign: 'left'}}>
@@ -126,6 +190,34 @@ class App extends Component {
                 <p className='powered-by' style={{fontSize: 10}}>
                   send me feedback <a target='_blank' rel="noopener noreferrer" style={{color: 'gray'}} href='https://twitter.com/adarbutel'>@adarbutel</a>
                 </p>
+              </div>
+              <div style={{marginTop: 45, textAlign: 'center'}}>
+                <p style={{fontSize: 10}}>
+                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>BTC: 18gqDMJQGR21AyR5FJ9wi6QofbD2CwpfPq
+                  </a>
+                </p>
+                <img style={{maxWidth: 200, backgroundColor: 'white', padding: 5}} src={btcQR} />
+              </div>
+              <div style={{marginTop: 45, textAlign: 'center'}}>
+                <p style={{fontSize: 10}}>
+                  <a href='https://etherscan.io/address/0xa1a79545df48860c475d8df4a2c947920ec309e6' target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>ETH: 0xA1a79545df48860c475D8Df4A2C947920EC309e6
+                  </a>
+                </p>
+                <img style={{backgroundColor: 'white', padding: 5}} src={ethQR} />
+              </div>
+              <div style={{marginTop: 45, textAlign: 'center'}}>
+                <p style={{fontSize: 10}}>
+                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>LTC: LN5WevGS9HmBdBpNWo9Mp9vGN2RAY2ZoPf
+                  </a>
+                </p>
+                <img style={{backgroundColor: 'white', padding: 5}} src={ltcQR} />
+              </div>
+              <div style={{marginTop: 45, textAlign: 'center'}}>
+                <p style={{fontSize: 10}}>
+                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>XMR: 47jyXhag5vNMZR6NcDmmifXK3AmMD6c3xYeAHXHSpM52gsisCbZfVpsgT5FFGADskffiQ68dwghUiC9mWGzT9aFZ1aFGLrr
+                  </a>
+                </p>
+                <img style={{backgroundColor: 'white', padding: 5}} src={xmrQR} />
               </div>
             </div>
           </tbody>
