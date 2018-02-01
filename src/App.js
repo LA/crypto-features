e997ec0b560b2f81f131c4266ffa7037eabc6a4e
Adar <theadarbutel@gmail.com>
2018-01-31 16:27:24 -0800

    update contribution flow and add bitcoin symbol

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ class App extends Component {
       feed: [],
       prices: [],
       globalData: {},
+      contributeOpen: false,
+      contributeButtonText: 'contribute'
     };
 
     document.title = 'Crypto Features -  Cryptocurrency Prices & Cryptocurrency News'
@@ -174,16 +176,16 @@ class App extends Component {
             <div style={{marginLeft: 'auto'}}>
               <div style={{marginTop: 15, marginLeft: 25, display: 'inline-block'}}>
                 <p style={{color: 'white', textAlign: 'left', fontSize: 10}}>
-                  Name: BTC Equiv ($USD) - $USD MKT Cap - % Change 24H
+                  Name: ₿BTC ($USD) - $USD MKT Cap - % Change 24H
                 </p>
                 {this.state.prices.map((coin, index) => {
                   const price = Number(coin.price_usd);
-                  console.log(coin.name, coin.price_btc);
+                  console.log(coin.name, ':', '$'+coin.price_usd, '₿'+coin.price_btc);
                   const coinPrice = (price > 1) ? format('#,###.00', price) : price;
 
                   const coinMarketCap = format('#,###.', coin.market_cap_usd);
 
-                  const btcEquiv = coin.price_btc;
+                  const btcEquiv = '₿'+coin.price_btc;
 
                   const pctChange = coin.percent_change_24h || '?';
                   let color = (pctChange[0] === '-') ? 'red' : 'green';
@@ -196,50 +198,67 @@ class App extends Component {
                     <p key={index} style={{color: 'white', textAlign: 'left', fontSize: 10}}>{index+1}. {coin.name}: {btcEquiv} (${coinPrice}) - ${coinMarketCap} - <a style={{color: color}} >{arrow}%</a> 24H</p>
                   );
                 })}
-                <p className='powered-by' style={{fontSize: 10, textAlign: 'left'}}>
+                <p style={{fontSize: 10, textAlign: 'left'}}>
                   powered by <a target='_blank' rel="noopener noreferrer" style={{color: 'gray'}} href='https://coinmarketcap.com/'>CoinMarketCap.com</a>
+                </p>
+                <p style={{fontSize: 10, textAlign: 'left'}}>
+                  <a target='_blank' rel="noopener noreferrer" style={{color: 'gray', textDecoration: 'underline', cursor: 'pointer'}}
+                  onClick={() => {
+                    this.setState({ contributeOpen: !this.state.contributeOpen });
+                    this.setState({
+                      contributeButtonText: (!this.state.contributeOpen) ? 'hide contribute' : 'contribute'
+                    })
+                  }}>{this.state.contributeButtonText}</a>
                 </p>
               </div>
               <div style={{marginTop: 25, marginLeft: 25, display: 'inline-block'}}>
-                <p className='powered-by' style={{fontSize: 10}}>
+                <p style={{fontSize: 10}}>
                   send me feedback <a target='_blank' rel="noopener noreferrer" style={{color: 'gray'}} href='https://twitter.com/adarbutel'>@adarbutel</a>
                 </p>
               </div>
-              <div style={{marginTop: 45, textAlign: 'left'}}>
-                <p style={{fontSize: 10}}>
-                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>Steem: @throne
-                  </a>
-                </p>
-                <img style={{maxWidth: 200, backgroundColor: 'white', padding: 5}} src={steemQR} />
-              </div>
-              <div style={{marginTop: 45, textAlign: 'left'}}>
-                <p style={{fontSize: 10}}>
-                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>BTC: 18gqDMJQGR21AyR5FJ9wi6QofbD2CwpfPq
-                  </a>
-                </p>
-                <img style={{maxWidth: 200, backgroundColor: 'white', padding: 5}} src={btcQR} />
-              </div>
-              <div style={{marginTop: 45, textAlign: 'left'}}>
-                <p style={{fontSize: 10}}>
-                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>ETH: 0xA1a79545df48860c475D8Df4A2C947920EC309e6
-                  </a>
-                </p>
-                <img style={{backgroundColor: 'white', padding: 5}} src={ethQR} />
-              </div>
-              <div style={{marginTop: 45, textAlign: 'left'}}>
-                <p style={{fontSize: 10}}>
-                  <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>LTC: LN5WevGS9HmBdBpNWo9Mp9vGN2RAY2ZoPf
-                  </a>
-                </p>
-                <img style={{backgroundColor: 'white', padding: 5}} src={ltcQR} />
-              </div>
-              <div style={{marginTop: 45, textAlign: 'left'}}>
-                <p style={{fontSize: 10}}>
-                  <a className='xmr-ad' target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>XMR: 47jyXhag5vNMZR6NcDmmifXK3AmMD6c3xYeAHXHSpM52gsisCbZfVpsgT5FFGADskffiQ68dwghUiC9mWGzT9aFZ1aFGLrr
-                  </a>
-                </p>
-                <img style={{backgroundColor: 'white', padding: 5}} src={xmrQR} />
-              </div>
+              {
+                this.state.contributeOpen
+                ?
+                <div>
+                  <div style={{marginTop: 45, textAlign: 'left'}}>
+                    <p style={{fontSize: 10}}>
+                      <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>Steem: @throne
+                      </a>
+                    </p>
+                    <img style={{maxWidth: 200, backgroundColor: 'white', padding: 5}} src={steemQR} />
+                  </div>
+                  <div style={{marginTop: 45, textAlign: 'left'}}>
+                    <p style={{fontSize: 10}}>
+                      <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>BTC: 18gqDMJQGR21AyR5FJ9wi6QofbD2CwpfPq
+                      </a>
+                    </p>
+                    <img style={{maxWidth: 200, backgroundColor: 'white', padding: 5}} src={btcQR} />
+                  </div>
+                  <div style={{marginTop: 45, textAlign: 'left'}}>
+                    <p style={{fontSize: 10}}>
+                      <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>ETH: 0xA1a79545df48860c475D8Df4A2C947920EC309e6
+                      </a>
+                    </p>
+                    <img style={{backgroundColor: 'white', padding: 5}} src={ethQR} />
+                  </div>
+                  <div style={{marginTop: 45, textAlign: 'left'}}>
+                    <p style={{fontSize: 10}}>
+                      <a target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>LTC: LN5WevGS9HmBdBpNWo9Mp9vGN2RAY2ZoPf
+                      </a>
+                    </p>
+                    <img style={{backgroundColor: 'white', padding: 5}} src={ltcQR} />
+                  </div>
+                  <div style={{marginTop: 45, textAlign: 'left'}}>
+                    <p style={{fontSize: 10}}>
+                      <a className='xmr-ad' target='_blank' rel='noopener noreferrer' style={{color: 'white'}}>XMR: 47jyXhag5vNMZR6NcDmmifXK3AmMD6c3xYeAHXHSpM52gsisCbZfVpsgT5FFGADskffiQ68dwghUiC9mWGzT9aFZ1aFGLrr
+                      </a>
+                    </p>
+                    <img style={{backgroundColor: 'white', padding: 5}} src={xmrQR} />
+                  </div>
+                </div>
+                :
+                null
+              }
             </div>
           </tbody>
         </table>
